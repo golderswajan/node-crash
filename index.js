efc88489fcbdabc4e00b821de38b38ca1391a331
Swajan Golder <swajan.cse.ku@gmail.com>
2025-05-31 23:25:35 +0600

    basic file handling added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {handleReqRes} = require('./helper/handleReqRes')
 const url = require('url');
 const {StringDecoder} = require('string_decoder');
 const environment = require('./helper/environments');
+const data = require('./lib/data')
 
 //app object
 const app = {};
@@ -13,8 +14,26 @@ const app = {};
 //         port : 3000
 // };
 
+// testing file system
+data.create('test', 'test_file',{'name':'Bangladesh','language':'Bangla'}, (err) => {
+        console.log('error is: '+err);
+})
+
+data.read('test', 'test_file', (err, data) => {
+        console.log('data:'+data+' error is: '+err);
+})
+
+// data.update('test', 'test_file', {'name':'India','language':'Bangla'}, (err) => {
+//         console.log('error is: ', err);
+// })
+
+// data.delete('test', 'test_file', (err, data) => {
+//         console.log('error is: '+err);
+// })
+
+
 app.createServer = () => {
-        const server = http.createServer(app.handleReqRes)
+        const server = http.createServer(app.handleReqRes);
         server.listen(environment.port, () => {
                 console.log(`environment: ${process.env.NODE_ENV}`);
                 console.log(`Server is listening at port: ${environment.port}`);
